69d5f15e416811f6ed7009523fa35e95862e05c9
costelus44 <165764808+costelus44@users.noreply.github.com>
2024-04-10 16:08:25 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
    * Tell Next.js where the `public` folder is.
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
-  assetPrefix: "/magic-next/pr-preview/,
+  assetPrefix: "/magic-next/pr-preview/",
   /**
    * Disable server-based image optimization.
    *
