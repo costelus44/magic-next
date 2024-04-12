595516304aa29ee50ca1192ab267f1fdfb6a6c03
costelus44 <costelus44@users.noreply.github.com>
2024-04-12 06:53:34 +0000

    Deploy preview for PR 49 🛫

---
+++ pr-preview/pr-49/_next/static/0FKqM0MsUd0cNBl6Ipd9i/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/955229c28454e75a.css","static/chunks/pages/index-3e7d2c7790300199.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
