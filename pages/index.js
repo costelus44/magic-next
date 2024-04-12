aa5256bd1150959205d87e2506a79765ddb292fc
costelus44 <165764808+costelus44@users.noreply.github.com>
2024-04-12 09:52:53 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
+          tests br [2] Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
